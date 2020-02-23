817fa498a0c18250f6418e4c1df578b1c7c64fbc
satokazu14 <satokazu.0910-14@ezweb.ne.jp>
2020-02-24 03:49:24 +0900

    画像のアップロード以外完成

--- app/Http/Controllers/MusicUploadController.php
+++ app/Http/Controllers/MusicUploadController.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Models\Artist;
 use Illuminate\Http\Request;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Genre;
 use App\Models\Music;
 use getID3;
@@ -16,23 +19,51 @@ class MusicUploadController extends Controller
     $genres = Genre::all();
     //アーティストの(ry
     $artists = Artist::all();
-    return view('music_upload', compact('genres', 'artists'));
+    //楽曲(ry
+    $musics = Music::all();
+    return view('music_upload', compact('genres', 'artists', 'musics'));
   }
 
   public function musicStore(Request $request)
   {
+    $file_name = $request->file('file')->getClientOriginalName();
+    if (app()->isLocal()) {
+      $request->file('file')->storeAs('public/music', $file_name);
+    } else {
+      Storage::disk('s3')->putFileAs('music/', $request->file('file'), $file_name, 'public');
+    }
     $getID3 = new getID3();
-    $music_info = $getID3->analyze("music/test.mp3");
-    //getid3_lib::CopyTagsToComments($music_info);
-    dd($music_info);
-    $music = Music::create([
-      'artist_id'   => $request->artist,
+    $getID3->setOption(array('encoding' => 'UTF-8'));
+    $music_info = $getID3->analyze('storage/music/' . $file_name);
+    getid3_lib::CopyTagsToComments($music_info);
+    /*
+      $music = Music::create([
+        'artist_id'   => $request->artist,
+        'genre_id'    => $request->genre,
+        'name'        => $request->name,
+        'time'        => $music_info['playtime_string'],
+        'price'       => $request->price,
+        'release_date' => $request->data,
+      ]);
+      */
+    return redirect()->route('music_upload')->with('message', 'musicアップロード成功！');
+  }
+
+  public function genreStore(Request $request)
+  {
+    Genre::create([
+      'name' => $request->name,
+    ]);
+    return redirect()->route('music_upload')->with('message', 'ジャンル登録成功！');
+  }
+
+  public function artistStore(Request $request)
+  {
+    Artist::create([
       'genre_id'    => $request->genre,
       'name'        => $request->name,
-      'time'        => $request->artist,
-      'price'       => $request->price,
-      'release_date'=> $request->date,
+      'description' => $request->detail,
     ]);
-    return view('music_upload', compact('genres', 'artists'));
+    return redirect()->route('music_upload')->with('message', 'アーティスト登録成功！');
   }
 }
--- resources/views/music_upload.blade.php
+++ resources/views/music_upload.blade.php
@@ -11,70 +11,214 @@
 
 <!-- content -->
 @section('content')
-<div class="container">
-  <h1 class="mb-2">楽曲登録</h1>
-  <form method="post" action="{{ url('music_upload/music_store') }}">
-    <div class="form-group">
-      <label>アーティスト</label>
-      <select class="custom-select" name="artist">
+<div id="app" class="container">
+  @if(session('message'))
+  <div class="alert alert-success mt-4" role="alert"><strong>{{ session('message') }}</strong></div>
+  @endif
+  <div id="toggle-btn" class="btn-group btn-group-toggle mt-5" data-toggle="buttons" @change="toggleChange">
+    <label class="btn btn-primary page-link d-inline-block" v-bind:class="{ active: isActive1 }">
+      <input type="radio" class="option" name="options" id="option1" value="1" checked> 楽曲
+    </label>
+    <label class="btn btn-primary page-link d-inline-block" v-bind:class="{ active: isActive2 }">
+      <input type="radio" class="option" name="options" id="option2" value="2"> ジャンル
+    </label>
+    <label class="btn btn-primary page-link d-inline-block" v-bind:class="{ active: isActive3 }">
+      <input type="radio" class="option" name="options" id="option3" value="3"> アーティスト
+    </label>
+  </div>
+  <div v-if="choice == 0">
+    <h1 class="mt-2 mb-2">楽曲登録</h1>
+    <form method="post" action="{{ url('music_upload/music_store') }}" enctype="multipart/form-data">
+      @csrf
+      <div class="form-group">
+        <label>アーティスト</label>
+        <select class="custom-select" name="artist">
+          @foreach($artists as $artist)
+          <option value="{{ $artist->id }}">{{ $artist->name }}</option>
+          @endforeach
+        </select>
+      </div>
+      <div class="form-group">
+        <label>ジャンル</label>
+        <select class="custom-select" name="genre">
+          @foreach($genres as $genre)
+          <option value="{{ $genre->id }}">{{ $genre->name }}</option>
+          @endforeach
+        </select>
+      </div>
+      <div class="form-group">
+        <label>曲名…ファイルを選ぶと自動で入力されます</label>
+        <input class="form-control" type="text" name="name" v-model="musicName" value="{{ old('name') }}">
+      </div>
+      <div class="form-group">
+        <label>価格</label>
+        <input class="form-control" type="number" name="price" value="{{ old('price') }}">
+      </div>
+      <div class="form-group">
+        <label>発売日</label>
+        <input class="form-control" type="date" name="data" value="{{ old('day') }}">
+      </div>
+      <label>音楽ファイル</label>
+      <div class="custom-file mb-2">
+        <input type="file" class="custom-file-input" id="customMfile" name="mfile" @change="mfileNameChange">
+        <label class="custom-file-label" for="customMfile" data-browse="参照">ファイル選択...</label>
+      </div>
+      <label>画像ファイル</label>
+      <div class="custom-file mb-2">
+        <input type="file" class="custom-file-input" id="customIfile" name="ifile" @change="ifileNameChange">
+        <label class="custom-file-label" for="customIfile" data-browse="参照">ファイル選択...</label>
+      </div>
+      <button class="btn btn-primary" type="submit">登録</button>
+    </form>
+    <table class="table table-bordered mt-2">
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>アーティスト</td>
+          <td>ジャンル</td>
+          <td>曲名</td>
+          <td>長さ</td>
+          <td>値段</td>
+          <td>発売日</td>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($musics as $music)
+        <tr>
+          <td>{{ $music->id }}</td>
+          <td>{{ $artists[$music->artist_id - 1]->name }}</td>
+          <td>{{ $genres[$music->genre_id - 1]->name }}</td>
+          <td>{{ $music->name }}</td>
+          <td>{{ $music->time }}</td>
+          <td>{{ $music->price }}</td>
+          <td>{{ $music->release_date }}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+  <div v-if="choice == 1">
+    <h1 class="mb-2 mt-2">ジャンル登録</h1>
+    <form method="post" action="{{ url('music_upload/genre_store') }}">
+      @csrf
+      <div class="form-group">
+        <label>ジャンル名</label>
+        <input class="form-control" type="text" name="name" value="{{ old('name') }}">
+      </div>
+      <button class="btn btn-primary" type="submit">登録</button>
+    </form>
+    <table class="table table-bordered mt-2">
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>ジャンル名</td>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($genres as $genre)
+        <tr>
+          <td>{{ $genre->id }}</td>
+          <td>{{ $genre->name }}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+  <div v-if="choice == 2">
+    <h1 class="mb-2 mt-2">アーティスト登録</h1>
+    <form class="mb-2" method="post" action="{{ url('music_upload/artist_store') }}">
+      @csrf
+      <div class="form-group">
+        <label>ジャンル</label>
+        <select class="custom-select" name="genre">
+          @foreach($genres as $genre)
+          <option value="{{ $genre->id }}">{{ $genre->name }}</option>
+          @endforeach
+        </select>
+      </div>
+      <div class="form-group">
+        <label>アーティスト名</label>
+        <input class="form-control" type="text" name="name" value="{{ old('name') }}">
+      </div>
+      <div class="form-group">
+        <label>アーティストの説明</label>
+        <textarea class="form-control" rows="3" name="detail">{{ old('detail') }}</textarea>
+      </div>
+      <button class="btn btn-primary" type="submit">登録</button>
+    </form>
+    <table class="table table-bordered mt-2">
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>ジャンル</td>
+          <td>アーティスト名</td>
+          <td>説明</td>
+        </tr>
+      </thead>
+      <tbody>
         @foreach($artists as $artist)
-        <option value="{{ $artist->id }}">{{ $artist->name }}</option>
+        <tr>
+          <td>{{ $artist->id }}</td>
+          <td>{{ $genres[$artist->genre_id - 1]->name }}</td>
+          <td>{{ $artist->name }}</td>
+          <td>{{ $artist->description }}</td>
+        </tr>
         @endforeach
-      </select>
-    </div>
-    <div class="form-group">
-      <label>ジャンル</label>
-      <select class="custom-select" name="genre">
-        @foreach($genres as $genre)
-        <option value="{{ $genre->id }}">{{ $genre->name }}</option>
-        @endforeach
-      </select>
-    </div>
-    <div class="form-group">
-      <label>曲名</label>
-      <input class="form-control" type="text" name="name" value="{{ old('name') }}">
-    </div>
-    <div class="form-group">
-      <label>価格</label>
-      <input class="form-control" type="number" name="price" value="{{ old('price') }}">
-    </div>
-    <div class="form-group">
-      <label>発売日</label>
-      <input class="form-control" type="date" name="day" value="{{ old('day') }}">
-    </div>
-    <button class="btn btn-primary" type="submit">登録</button>
-  </form>
-
-  <h1 class="mb-2 mt-5">ジャンル登録</h1>
-  <form method="post" action="{{ url('music_upload/genre_store') }}">
-    <div class="form-group">
-      <label>ジャンル名</label>
-      <input class="form-control" type="text" name="name" value="{{ old('name') }}">
-    </div>
-    <button class="btn btn-primary" type="submit">登録</button>
-  </form>
-
-  <h1 class="mb-2 mt-5">アーティスト登録</h1>
-  <form class="mb-2" method="post" action="{{ url('music_upload/artist_store') }}">
-    <div class="form-group">
-      <label>ジャンル</label>
-      <select class="custom-select" name="genre">
-        @foreach($genres as $genre)
-        <option value="{{ $genre->id }}">{{ $genre->name }}</option>
-        @endforeach
-      </select>
-    </div>
-    <div class="form-group">
-      <label>アーティスト名</label>
-      <input class="form-control" type="text" name="name" value="{{ old('name') }}">
-    </div>
-    <button class="btn btn-primary" type="submit">登録</button>
-  </form>
+      </tbody>
+    </table>
+  </div>
 </div>
+<style type="text/css">
+  .custom-file {
+    overflow: hidden;
+  }
+
+  .custom-file-label {
+    white-space: nowrap;
+  }
+</style>
+<script type="text/javascript">
+  new Vue({
+    el: '#app',
+    data: {
+      musicName:'',
+      choice:0,
+      isActive1:true,
+      isActive2:false,
+      isActive3:false,
+    },
+    methods: {
+      mfileNameChange(e) {
+        e.target.labels[0].innerText = e.target.files[0].name;
+        this.musicName = e.target.files[0].name.split( '.' )[0];
+      },
+      ifileNameChange(e) {
+        e.target.labels[0].innerText = e.target.files[0].name;
+      },
+      toggleChange(e) {
+        if(e.target.value == 1){
+          this.isActive1 = true;
+          this.isActive2 = false;
+          this.isActive3 = false;
+          this.choice = 0;
+        } else if(e.target.value == 2){
+          this.isActive1 = false;
+          this.isActive2 = true;
+          this.isActive3 = false;
+          this.choice = 1;
+        } else {
+          this.isActive1 = false;
+          this.isActive2 = false;
+          this.isActive3 = true;
+          this.choice = 2;
+        }
+      }
+    }
+  });
+</script>
 @endsection
 
 <!-- footer -->
 @include('common.footer')
-
-<script type="text/javascript">
-</script>
