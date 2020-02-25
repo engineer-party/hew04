84c724080ad8b311a22902ca7d6c96b3d3dc09d2
Seima-Yonesho <52072148+Seima-Yonesho@users.noreply.github.com>
2020-02-26 01:42:26 +0900

    layout ajax

--- resources/views/playlist.blade.php
+++ resources/views/playlist.blade.php
@@ -27,5 +27,18 @@
       <button class="play-btn"></button>
     </div>
     <!-- END -->
+    
+    <!-- プレイリスト -->
+    <div class="playlist-content">
+      <div class="img">
+        <img src="{{ asset('img/cheep-trick.jpg') }}" alt="">
+        <img src="{{ asset('img/joan-jett.jpg') }}" alt="">
+        <img src="{{ asset('img/plus.jpg') }}" alt="">
+        <img src="{{ asset('img/sex-pistols.jpg') }}" alt="">
+      </div>
+      <p class="title">Rock-Hot</p>
+      <button class="play-btn"></button>
+    </div>
+    <!-- END -->
   </div>
 </div>
