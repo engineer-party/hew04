b13eda36a30667f1441281b6e8dc6baa067ef49f
Seima-Yonesho <52072148+Seima-Yonesho@users.noreply.github.com>
2020-02-25 18:28:02 +0900

    layout library

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -5,6 +5,7 @@
 @section('style')
 <link rel="stylesheet" href="{{asset('css/link.css')}}" />
 <link rel="stylesheet" href="{{asset('css/hbg.css')}}" />
+<link rel="stylesheet" href="{{asset('css/playlist.css')}}" />
 
 <script src="{{ asset('js/library.js') }}" defer></script>
 <script src="{{ asset('js/hbg.js') }}" defer></script>
@@ -18,35 +19,56 @@
 <!-- content -->
 @section('content')
 <div id="contents">
-<nav id="navber">
-  <ul>
-    <li><a class="library-link ajax-action" 
-    v-on:click='activetab=1'
-    v-bind:class="[ activetab === 1 ? 'active' : '' ]">プレイリスト</a></li>
-    <li><a class="library-link ajax-action" 
-    v-on:click='activetab=2'
-    v-bind:class="[ activetab === 2 ? 'active' : '' ]">曲</a></li>
-    <li><a class="library-link ajax-action" 
-    v-on:click='activetab=3'
-    v-bind:class="[ activetab === 3 ? 'active' : '' ]">アーティスト</a></li>
-    <li><a class="library-link ajax-action" 
-    v-on:click='activetab=4'
-    v-bind:class="[ activetab === 4 ? 'active' : '' ]">アルバム</a></li>
-  </ul>
-</nav>
+  <nav id="navber">
+    <ul>
+      <li><a class="library-link ajax-action1" v-on:click='activetab=1' v-bind:class="[ activetab === 1 ? 'active' : '' ]">プレイリスト</a></li>
+      <li><a class="library-link ajax-action2" v-on:click='activetab=2' v-bind:class="[ activetab === 2 ? 'active' : '' ]">曲</a></li>
+      <li><a class="library-link ajax-action3" v-on:click='activetab=3' v-bind:class="[ activetab === 3 ? 'active' : '' ]">アーティスト</a></li>
+      <li><a class="library-link ajax-action4" v-on:click='activetab=4' v-bind:class="[ activetab === 4 ? 'active' : '' ]">アルバム</a></li>
+    </ul>
+  </nav>
 
-<section
-class="activeobj1 box"
-v-bind:class="[ activetab === 1 ? 'activeobj' : '' ]"></section>
-<section
-class="activeobj2 box"
-v-bind:class="[ activetab === 2 ? 'activeobj' : '' ]"></section>
-<section
-class="activeobj3 box"
-v-bind:class="[ activetab === 3 ? 'activeobj' : '' ]"></section>
-<section
-class="activeobj4 box"
-v-bind:class="[ activetab === 4 ? 'activeobj' : '' ]"></section>
+  <section class="activeobj1 box" v-bind:class="[ activetab === 1 ? 'activeobj' : '' ]">
+    <!-- ajax -->
+    <div class="contents">
+      <!-- プレイリスト -->
+      <div class="playlist-content">
+        <div class="img">
+<!--
+          <ul>
+            <li><img src="{{ asset('img/cheep-trick.jpg') }}" alt=""></li>
+            <li><img src="{{ asset('img/joan-jett.jpg') }}" alt=""></li>
+            <li><img src="{{ asset('img/plus.jpg') }}" alt=""></li>
+            <li><img src="{{ asset('img/sex-pistols.jpg') }}" alt=""></li>
+          </ul>
+-->
+        </div>
+        <div class="text">
+          <p class="title">Rock-Hot</p>
+        </div>
+      </div>
+      <!-- END -->
+      
+      <!-- プレイリスト -->
+      <div class="playlist-content">
+        <div class="img">
+          <ul>
+            <li><img src="{{ asset('img/cheep-trick.jpg') }}" alt=""></li>
+            <li><img src="{{ asset('img/joan-jett.jpg') }}" alt=""></li>
+            <li><img src="{{ asset('img/plus.jpg') }}" alt=""></li>
+            <li><img src="{{ asset('img/sex-pistols.jpg') }}" alt=""></li>
+          </ul>
+        </div>
+        <div class="text">
+          <p class="title">Rock-Hot</p>
+        </div>
+      </div>
+      <!-- END -->
+    </div>
+  </section>
+  <section class="activeobj2 box" v-bind:class="[ activetab === 2 ? 'activeobj' : '' ]"></section>
+  <section class="activeobj3 box" v-bind:class="[ activetab === 3 ? 'activeobj' : '' ]"></section>
+  <section class="activeobj4 box" v-bind:class="[ activetab === 4 ? 'activeobj' : '' ]"></section>
 
 </div>
 @endsection
@@ -59,7 +81,8 @@ v-bind:class="[ activetab === 4 ? 'activeobj' : '' ]"></section>
     margin: 0;
     padding: 0;
   }
-/*
+
+  /*
   #wrapper {
     position: fixed;
     width: 100%;
@@ -70,17 +93,20 @@ v-bind:class="[ activetab === 4 ? 'activeobj' : '' ]"></section>
   #app .form {
     display: none;
   }
+
   #app {
     width: 100%;
     background-color: #ff5757;
     position: relative;
     z-index: 100;
   }
-  #app .menu-trigger{
+
+  #app .menu-trigger {
     top: 22px;
     left: 30px;
   }
-  #app h1{
+
+  #app h1 {
     padding: 20px;
     padding-left: 65px;
     padding-bottom: 40px;
@@ -88,42 +114,47 @@ v-bind:class="[ activetab === 4 ? 'activeobj' : '' ]"></section>
     font-size: 1.2em;
     letter-spacing: 4px;
   }
+
   #navber {
     position: relative;
     width: 100%;
     background-color: #ff5757;
     overflow-x: scroll;
     justify-content: space-around;
-    z-index: 100;
-    box-shadow: 2px 2px 3px rgba(0,0,0,0.3);
+    z-index: 1;
+    box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.3);
+/*    margin-bottom: -200px;*/
   }
-  #navber ul{
+
+  #navber ul {
     display: flex;
     width: 400px;
 
   }
-  #navber li{
+
+  #navber li {
     color: white;
-/*    background-color: aqua;*/
-/*    border: solid black 1px;*/
+    /*    background-color: aqua;*/
+    /*    border: solid black 1px;*/
     text-align: center;
     font-size: 0.8em;
     width: calc(100% / 4);
   }
+
   .library-link {
     display: block;
     line-height: 40px;
     height: 40px;
     padding: 0;
     margin: 0;
-/*    border: solid black 1px;*/
+    /*    border: solid black 1px;*/
     width: 100%;
     font-size: 0.8em;
     background-color: #ff5757;
     color: white;
-/*    left: 10px;*/
+    /*    left: 10px;*/
   }
-  
+
   .box {
     width: 95%;
     position: absolute;
@@ -133,23 +164,36 @@ v-bind:class="[ activetab === 4 ? 'activeobj' : '' ]"></section>
     height: 100vh;
     transition: transform 350ms cubic-bezier(0, 0, 0.2, 1) 0ms;
   }
+
   .activeobj1 {
     background-color: aqua;
   }
+
   .activeobj2 {
     background-color: blueviolet;
   }
+
   .activeobj3 {
     background-color: chartreuse;
   }
+
   .activeobj4 {
     background-color: darkorange;
+  }
+  .contents {
+    width: 100%;
+    height: auto;
+    padding-top: 140px;
+    display: grid;
+    grid-gap: 0px 10px;
+    grid-template-columns: calc(50% - 5px) calc(50% - 5px);
   }
 
   /*----- addclass -----*/
   .active {
     border-bottom: solid white 3px;
   }
+
   .activeobj {
     transform: translate(-50%);
   }
--- resources/views/playlist.blade.php
+++ resources/views/playlist.blade.php
@@ -1,85 +1,16 @@
-@extends('layout.layout')
-
-<!-- head -->
-@section('title', 'PlayList')
-@section('style')
-<link rel="stylesheet" href="{{asset('css/link.css')}}" />
-<link rel="stylesheet" href="{{asset('css/hbg.css')}}" />
-
-<script src="{{ asset('js/playlist.js') }}" defer></script>
-<script src="{{ asset('js/hbg.js') }}" defer></script>
-
-@endsection
-@include('common.head')
-
-<!-- header -->
-@include('common.header')
-
-<!-- content -->
-@section('content')
-<p>うんこ</p>
-@endsection
-
-<!-- footer -->
-@include('common.footer')
-
-<style type="text/css">
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  #wrapper {
-    position: fixed;
-    width: 100%;
-    height: 100vh;
-    background-color: aliceblue;
-  }
-  #app .form {
-    display: none;
-  }
-  #app {
-    width: 100%;
-    background-color: #ff5757;
-  }
-  #app .menu-trigger{
-    top: 22px;
-    left: 30px;
-  }
-  #app h1{
-    padding: 20px;
-    padding-left: 65px;
-    padding-bottom: 40px;
-    color: white;
-    font-size: 1.2em;
-    letter-spacing: 4px;
-  }
-  #navber {
-    position: fixed;
-    width: 100%;
-    background-color: #ff5757;
-    overflow-x: scroll;
-  }
-  #navber ul{
-    display: flex;
-    width: 400px;
-  }
-  #navber li{
-    color: white;
-    line-height: 40px;
-    height: 40px;
-    width: 100px;
-/*    background-color: aqua;*/
-/*    border: solid black 1px;*/
-    text-align: center;
-    font-size: 0.8em;
-  }
-  #navber li:nth-child(1){
-    border-bottom: solid white 3px;
-  }
-  p {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-  }
-</style>
+<div class="contents">
+  
+  <!-- プレイリスト -->
+  <div class="playlist-content">
+    <div class="img">
+      <ul>
+        <li><img src="{{ asset('img/cheep-trick.jpg') }}" alt=""></li>
+        <li><img src="{{ asset('img/joan-jett.jpg') }}" alt=""></li>
+        <li><img src="{{ asset('img/plus.jpg') }}" alt=""></li>
+        <li><img src="{{ asset('img/sex-pistols.jpg') }}" alt=""></li>
+      </ul>
+    </div>
+  </div>
+  <!-- END -->
+  
+</div>
