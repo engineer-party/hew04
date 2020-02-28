9635ed8445526082ac0cf5f26a51dc55299f4502
Seima-Yonesho <52072148+Seima-Yonesho@users.noreply.github.com>
2020-02-28 21:31:05 +0900

    layout top page #2

--- public/js/search.js
+++ public/js/search.js
@@ -8,4 +8,15 @@ $(function(){
 //    $('#contents').animate({transform: 'translate(0,-100vh)'}, 300);
       $('#contents').delay(500).fadeIn(500);
   });
+  $('#form').hammer().on('tap', function (){
+    if(headOpen == true){
+      $('.search').addClass('activecategory');
+    }
+  });
+  
+  $('.menu-trigger').hammer().on('tap', function (){
+    if(headOpen == false){
+      $('.search').removeClass('activecategory');
+    }
+  });
 });
