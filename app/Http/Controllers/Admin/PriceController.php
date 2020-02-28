066098377b3eb3c4ca70512d967b4d951963c80a
seiya12 <t72txe6n@i.softbank.jp>
2020-02-28 19:58:05 +0900

    値引系DB周り＋form

--- app/Http/Controllers/Admin/PriceController.php
+++ app/Http/Controllers/Admin/PriceController.php
@@ -4,11 +4,35 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Genre;
+use App\Models\Music;
+use App\Models\Artist;
 
 class PriceController extends Controller
 {
     public function index()
     {
-        return view('Admin\price',compact(''));
+        $artists = Artist::All();
+        $musics = Music::All();
+        return view('Admin\price',compact('artists','musics'));
+    }
+
+    public function artist(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            'artist' => 'required',
+            'price' => 'required|alpha_dash',
+            'content' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator->errors())->withInput($req->all);
+        }
+
+        return redirect()->route('information')->with('message', 'お知らせ送信完了！');
+    }
+
+    public function music(Request $req)
+    {
+
     }
 }
--- resources/views/admin/price.blade.php
+++ resources/views/admin/price.blade.php
@@ -12,44 +12,88 @@
 
 <!-- content -->
 @section('content')
-<h3><i class="fa fa-angle-right"></i> Discount Form</h3>
-<!-- BASIC FORM VALIDATION -->
+<h3><i class="fa fa-angle-right"></i> 価格設定</h3>
+@if(session('message'))
+  <div class="alert alert-success mt-4" role="alert"><strong>{{ session('message') }}</strong></div>
+@endif
 <div class="row mt">
-  <div class="col-lg-12">
-    <h4><i class="fa fa-angle-right"></i> Artist</h4>
+  <div class="col-lg-6 col-md-6 col-sm-6">
     <div class="form-panel">
-      <form role="form" class="form-horizontal style-form">
-        <div class="form-group has-success">
-          <label class="col-lg-2 control-label">Artist Name</label>
-          <div class="col-lg-10">
-            <input type="text" placeholder="" id="f-name" class="form-control">
-            <p class="help-block">Successfully done</p>
-          </div>
-        </div>
-        <div class="form-group has-error">
-          <label class="col-lg-2 control-label">Discount Price</label>
-          <div class="col-lg-10">
-            <input type="text" placeholder="" id="l-name" class="form-control">
-            <p class="help-block">Aha you gave a wrong info</p>
-          </div>
-        </div>
-        <div class="form-group has-warning">
-          <label class="col-lg-2 control-label">Email</label>
-          <div class="col-lg-10">
-            <input type="email" placeholder="" id="email2" class="form-control">
-            <p class="help-block">Something went wrong</p>
-          </div>
+      <h4 class="title">アーティスト名</h4>
+      <div id="message"></div>
+      <form class="contact-form php-mail-form" role="form" action="price/artist" method="POST">
+        <div class="form-group">
+          <select name="artist" class="form-control">
+            <option value="">アーティスト名選択</option>
+            @foreach ($artists as $artist)
+            <option value="{{ $artist->id }}" @if(old('artist')==$artist->id) selected  @endif>{{ '#' . $artist->id . '　' . $artist->name }}</option>
+            @endforeach
+          </select>
         </div>
         <div class="form-group">
-          <div class="col-lg-offset-2 col-lg-10">
-            <button class="btn btn-theme" type="submit">Submit</button>
-          </div>
+          <input type="num" name="price" class="form-control" id="price" value="{{ old ('price') }}" placeholder="値引額" >
+          @if($errors)
+          <p class="help-block">{{$errors->first('price')}}</p>
+          @endif
         </div>
+        <div class="form-group">
+          <select name="period" class="form-control">
+            <option value="">アーティスト名選択</option>
+            <option value="5d">5日</option>
+            <option value="7d">7日</option>
+            <option value="10d">10日</option>
+            <option value="30d">30日</option>
+            <option value="1m">実演用 - 1分</option>
+            <option value="3m">実演用 - 3分</option>
+          </select>
+        </div>
+
+        <div class="form-send">
+          <button type="submit" class="btn btn-large btn-primary">変更する</button>
+        </div>
+
       </form>
     </div>
-    <!-- /form-panel -->
   </div>
-  <!-- /col-lg-12 -->
+
+  <div class="col-lg-6 col-md-6 col-sm-6">
+    <div class="form-panel">
+      <h4 class="title">楽曲名</h4>
+      <div id="message"></div>
+      <form class="contact-form php-mail-form" role="form" action="price/music" method="POST">
+        <div class="form-group">
+          <select name="music" class="form-control">
+            <option value="">楽曲名選択</option>
+            @foreach ($musics as $music)
+            <option value="{{ $music->id }}" @if(old('music')==$music->id) selected  @endif>{{ '#' . $music->id . '　' . $music->name }}</option>
+            @endforeach
+          </select>
+        </div>
+        <div class="form-group">
+          <input type="num" name="price" class="form-control" id="price" value="{{ old ('price') }}" placeholder="値引額" >
+          @if($errors)
+          <p class="help-block">{{$errors->first('price')}}</p>
+          @endif
+        </div>
+        <div class="form-group">
+          <select name="period" class="form-control">
+            <option value="">キャンペーン期間選択</option>
+            <option value="5d">5日</option>
+            <option value="7d">7日</option>
+            <option value="10d">10日</option>
+            <option value="30d">30日</option>
+            <option value="1m">実演用 - 1分</option>
+            <option value="3m">実演用 - 3分</option>
+          </select>
+        </div>
+
+        <div class="form-send">
+          <button type="submit" class="btn btn-large btn-primary">変更する</button>
+        </div>
+
+      </form>
+    </div>
+  </div>
 </div>
 <!-- /row -->
 @endsection
