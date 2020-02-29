b890b5203c1e0561407a9b43032a83eb90197219
satokazu14 <satokazu.0910-14@ezweb.ne.jp>
2020-02-29 17:21:49 +0900

    Update MusicUploadController.php

--- app/Http/Controllers/Admin/MusicUploadController.php
+++ app/Http/Controllers/Admin/MusicUploadController.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Controller;
 use getID3;
 use getid3_lib;
 use falahati\PHPMP3\MpegAudio;
+
 class MusicUploadController extends Controller
 {
   public function index()
@@ -24,7 +25,7 @@ class MusicUploadController extends Controller
     $musics = Music::all();
 
     $page = true;
-    return view('Admin/music_upload', compact('genres', 'artists', 'musics','page'));
+    return view('Admin/music_upload', compact('genres', 'artists', 'musics', 'page'));
   }
 
   public function musicStore(Request $request)
@@ -36,26 +37,16 @@ class MusicUploadController extends Controller
     //sampleを取得するためのやつ
     $mp3 = new MpegAudio();
 
-    //local環境と本番環境で処理を変える
-    //本番環境の方はまだ動くか分かりません
-    if (app()->isLocal()) {
-      $request->file('musicfile')->storeAs('public/music', $mp3_file_name);
-      $request->file('imgfile')->storeAs('public/image', $img_file_name);
-      $mp3->fromFile('storage/music/'.$mp3_file_name)->trim(10, 30)->saveFile('storage/sample/sample_'.$mp3_file_name);
-    } else {
-      Storage::disk('s3')->putFileAs('music/', $request->file('musicfile'), $mp3_file_name, 'public');
-      Storage::disk('s3')->putFileAs('image/', $request->file('imgfile'), $img_file_name, 'public');
-      Storage::disk('s3')->putFileAs('sample/', $mp3->fromFile(Storage::disk('s3')->url('music/'.$mp3_file_name))->trim(10, 30), 'sample_'.$mp3_file_name, 'public');
-    }
+    //一旦ローカルに保存
+    $request->file('musicfile')->storeAs('public/music', $mp3_file_name);
 
     //mp3から再生時間の取得
     $getID3 = new getID3();
     $getID3->setOption(array('encoding' => 'UTF-8'));
     $music_info = $getID3->analyze('storage/music/' . $mp3_file_name);
     getid3_lib::CopyTagsToComments($music_info);
-
-    //DBにぶち込む
     /*
+    //DBにぶち込む
       $music = Music::create([
         'artist_id'    => $request->artist,
         'name'         => $request->name,
@@ -64,8 +55,12 @@ class MusicUploadController extends Controller
         'img_url'      => 
         'music_url'    => 
         'sample_url'   => 
-      ]);
-      */
+      ]);*/
+
+    Storage::disk('s3')->putFileAs('music/', $request->file('musicfile'), $mp3_file_name, 'public');
+    Storage::disk('s3')->putFileAs('image/', $request->file('imgfile'), $img_file_name, 'public');
+    Storage::disk('s3')->putFileAs('sample/', $mp3->fromFile(Storage::disk('s3')->url('music/' . $mp3_file_name))->trim(10, 30), 'sample_' . $mp3_file_name, 'public');
+
     return redirect()->route('admin/music_upload')->with('message', 'musicアップロード成功！');
   }
 
