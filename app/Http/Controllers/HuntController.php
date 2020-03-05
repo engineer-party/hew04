fa8fcfb938d4b35ef0f22b1271f239ebe5339dc3
Toshi7031 <7031.toshi@gmail.com>
2020-03-06 01:58:56 +0900

    test

--- app/Http/Controllers/HuntController.php
+++ app/Http/Controllers/HuntController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
-
 class HuntController extends Controller
 {
 
     public function index()
     {
-        $googleApi = "AIzaSyAdgA4uMkD2gNnAWjutaqZrvdQTIyUE3UY";
+        $api_key = "AIzaSyAdgA4uMkD2gNnAWjutaqZrvdQTIyUE3UY";
         $user = Auth::user();
         return view('hunt',compact('user'));
     }
