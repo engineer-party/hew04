40761ac4757f87ac21938c800209e526604cf5ed
satokazu14 <satokazu.0910-14@ezweb.ne.jp>
2020-03-06 05:06:37 +0900

    楽曲の複数選択

--- app/Http/Controllers/Admin/PriceController.php
+++ app/Http/Controllers/Admin/PriceController.php
@@ -12,119 +12,118 @@ use Carbon\Carbon;
 
 class PriceController extends Controller
 {
-    public function index()
-    {
-        $pars = [5,10,15,20,25,30,35,40,45,50];
-        $artists = Artist::All();
-        $musics = Music::All();
-        return view('admin/price',compact('artists','musics','pars'));
+  public function index()
+  {
+    $pars = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+    $artists = Artist::All();
+    $musics = Music::All();
+    return view('admin/price', compact('artists', 'musics', 'pars'));
+  }
+
+  public function salesArtist($artist_id)
+  {
+    return redirect()->route('price')->with('artist_id', $artist_id);
+  }
+
+  public function salesMusic($music_id)
+  {
+    return redirect()->route('price')->with('music_id', $music_id);
+  }
+
+  public function artist(Request $req)
+  {
+    $validator = Validator::make($req->all(), [
+      'artist' => 'required',
+      'name_artist' => 'required',
+      'content_artist' => 'required',
+      'artist_discount' => 'required',
+      'artist_period' => 'required',
+    ]);
+    if ($validator->fails()) {
+      return redirect()->back()->withErrors($validator->errors())->withInput($req->all);
+    }
+    // 終了日
+    if (mb_substr($req->artist_period, -1) == 'd') {
+      $today = Carbon::today(); // 今日の00時00分
+      $end_date_time = $today->addDays(substr($req->artist_period, 0, -1));
+    } else {
+      $now = Carbon::now(); // 現在日時
+      $end_date_time = $now->addMinutes(substr($req->artist_period, 0, -1));
+    }
+    // キャンペーン保存
+    $musics = Artist::find($req->artist)->musics()->get();
+    foreach ($musics as $music) {
+      if (Campaign::where('music_id', $music->id)->exists()) {
+        Campaign::where('music_id', $music->id)->delete();
+      }
+      $campaign = new Campaign;
+      $campaign->music_id = $music->id;
+      $campaign->name = $req->name_artist;
+      $campaign->content = $req->content_artist;
+      $campaign->discount = $req->artist_discount;
+      $campaign->end_date_time = $end_date_time;
+      $campaign->save();
     }
 
-    public function salesArtist($artist_id)
-    {
-        return redirect()->route('price')->with('artist_id', $artist_id);
+    return redirect()->route('price')->with('message', 'アーティスト値下げ完了！');
+  }
+
+  public function music(Request $req)
+  {
+    $validator = Validator::make($req->all(), [
+      'musics' => 'required',
+      'name_music' => 'required',
+      'content_music' => 'required',
+      'music_discount' => 'required',
+      'music_period' => 'required',
+    ]);
+    if ($validator->fails()) {
+      return redirect()->back()->withErrors($validator->errors())->withInput($req->all);
+    }
+    // 終了日
+    if (mb_substr($req->music_period, -1) == 'd') {
+      $today = Carbon::today(); // 今日の00時00分
+      $end_date_time = $today->addDays(substr($req->music_period, 0, -1));
+    } else {
+      $now = Carbon::now(); // 現在日時
+      $end_date_time = $now->addMinutes(substr($req->music_period, 0, -1));
     }
 
-    public function salesMusic($music_id)
-    {
-        return redirect()->route('price')->with('music_id', $music_id);
+    if (in_array('all', $req->musics, true)) {
+      $musics = Music::All();
+      foreach ($musics as $music) {
+        if (Campaign::where('music_id', $music->id)->exists()) {
+          Campaign::where('music_id', $music->id)->delete();
+        }
+
+        $campaign = new Campaign;
+        $campaign->music_id = $music->id;
+        $campaign->name = $req->name_music;
+        $campaign->content = $req->content_music;
+        $campaign->discount = $req->music_discount;
+        $campaign->end_date_time = $end_date_time;
+        $campaign->save();
+      }
+    } else {
+      foreach ($req->musics as $music_val) {
+        if ($music_val != 0) {
+          // キャンペーン保存
+          $music = Music::find($music_val);
+          if (Campaign::where('music_id', $music->id)->exists()) {
+            Campaign::where('music_id', $music->id)->delete();
+          }
+
+          $campaign = new Campaign;
+          $campaign->music_id = $music->id;
+          $campaign->name = $req->name_music;
+          $campaign->content = $req->content_music;
+          $campaign->discount = $req->music_discount;
+          $campaign->end_date_time = $end_date_time;
+          $campaign->save();
+        }
+      }
     }
 
-    public function artist(Request $req)
-    {
-        $validator = Validator::make($req->all(), [
-            'artist' => 'required',
-            'name_artist' => 'required',
-            'content_artist' => 'required',
-            'artist_discount' => 'required',
-            'artist_period' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator->errors())->withInput($req->all);
-        }
-        // 終了日
-        if (mb_substr($req->artist_period, -1) == 'd'){
-            $today = Carbon::today(); // 今日の00時00分
-            $end_date_time = $today->addDays(substr($req->artist_period, 0, -1));
-        } 
-        else {
-            $now = Carbon::now(); // 現在日時
-            $end_date_time = $now->addMinutes(substr($req->artist_period, 0, -1));
-        }
-        // キャンペーン保存
-        $musics = Artist::find($req->artist)->musics()->get();
-        foreach ($musics as $music){
-            if (Campaign::where('music_id',$music->id)->exists()) {
-                Campaign::where('music_id',$music->id)->delete();
-            }
-            $campaign = new Campaign;
-            $campaign->music_id = $music->id;
-            $campaign->name = $req->name_artist;
-            $campaign->content = $req->content_artist;
-            $campaign->discount = $req->artist_discount;
-            $campaign->end_date_time = $end_date_time;
-            $campaign->save();
-        }
-
-        return redirect()->route('price')->with('message', 'アーティスト値下げ完了！');
-    }
-
-    public function music(Request $req)
-    {
-        $validator = Validator::make($req->all(), [
-            'music' => 'required',
-            'name_music' => 'required',
-            'content_music' => 'required',
-            'music_discount' => 'required',
-            'music_period' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator->errors())->withInput($req->all);
-        }
-        // 終了日
-        if (mb_substr($req->music_period, -1) == 'd'){
-            $today = Carbon::today(); // 今日の00時00分
-            $end_date_time = $today->addDays(substr($req->music_period, 0, -1));
-        } 
-        else {
-            $now = Carbon::now(); // 現在日時
-            $end_date_time = $now->addMinutes(substr($req->music_period, 0, -1));
-        }
-        
-        if (is_numeric($req->music)){
-            // キャンペーン保存
-            $music = Music::find($req->music);
-            if (Campaign::where('music_id',$music->id)->exists()) {
-                Campaign::where('music_id',$music->id)->delete();
-            }
-
-            $campaign = new Campaign;
-            $campaign->music_id = $music->id;
-            $campaign->name = $req->name_music;
-            $campaign->content = $req->content_music;
-            $campaign->discount = $req->music_discount;
-            $campaign->end_date_time = $end_date_time;
-            $campaign->save();
-        }
-        else {
-            if ($req->music == 'all'){
-                $musics = Music::All();
-                foreach ($musics as $music){
-                    if (Campaign::where('music_id',$music->id)->exists()) {
-                        Campaign::where('music_id',$music->id)->delete();
-                    }
-
-                    $campaign = new Campaign;
-                    $campaign->music_id = $music->id;
-                    $campaign->name = $req->name_music;
-                    $campaign->content = $req->content_music;
-                    $campaign->discount = $req->music_discount;
-                    $campaign->end_date_time = $end_date_time;
-                    $campaign->save();
-                }
-            }
-        }
-
-        return redirect()->route('price')->with('message', '楽曲値下げ完了！');
-    }
+    return redirect()->route('price')->with('message', '楽曲値下げ完了！');
+  }
 }
