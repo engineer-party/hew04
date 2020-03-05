cf33faca6c5d6eba7d247be23e07f89c35842ab1
satokazu14 <satokazu.0910-14@ezweb.ne.jp>
2020-03-06 00:11:45 +0900

    stripeのデフォルトクソださボタンとの戦闘の末勝利

--- resources/views/music-detail.blade.php
+++ resources/views/music-detail.blade.php
@@ -31,28 +31,26 @@
     <li><span class="value">{{ substr($music->time, 0, 5) }}</span><br><span class="text-in">再生時間</span></li>
     </ul>
   </div>
-  <div v-if="{{ $point }} > {{ $music->price }}">
-    <form method="post" action="{{ url('music/buy') }}">
-      @csrf
-        <div is="script"
-          src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-          data-key="{{ env('STRIPE_KEY') }}"
-          data-amount="{{ $music->price }}"
-          data-name="Hunting Music"
-          data-label="購入"
-          data-description="ポイントの購入"
-          data-image="{{ $image_path }}"
-          data-locale="auto"
-          data-currency="JPY">
-        </div>
-        <input type="hidden" name="value" value="">
-        <button class="btn buy-btn"><span class="point-icon">P</span>{{ $music->price }}</button>
-      </form>
-  </div>
-  <div v-else>
-
-  </div>
+  @if($point >= $music->price)
+  <form method="post" action="{{ url('music/buy') }}">
+    @csrf
+      <script
+        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+        data-key="{{ env('STRIPE_KEY') }}"
+        data-amount="{{ $music->price }}"
+        data-name="Hunting Music"
+        data-label="購入"
+        data-description="楽曲の購入"
+        data-image="{{ $image_path }}"
+        data-locale="auto"
+        data-currency="JPY">
+      </script>
+      <input type="hidden" name="value" value="">
+      <button class="btn buy-btn"><span class="point-icon">P</span>{{ $music->price }}</button>
+    </form>
+  @else
     <button class="btn buy-btn"><span class="point-icon">P</span>{{ $music->price }}</button>
+  @endif
   </div>
 </article>
 @endsection
@@ -150,12 +148,83 @@
     height: 50px;
     color: #ff5757;
     background-color: white;
-    margin-top: 50px;
     font-size: 1.0em;
+    top: -50px;
+    z-index: 2;
   }
   
   .text {
     padding-top: 10px;
   }
   
+  .stripe-button-el {
+  display: inline-block !important;
+  cursor: pointer !important;
+  width: 90% !important;
+  height: 50px !important;
+  background-color: white !important;
+  color: #ff5757 !important;
+  border-radius: 0px !important;
+  font-weight: 400 !important;
+  text-align: center !important;
+  white-space: nowrap !important;
+  vertical-align: middle !important;
+  -webkit-user-select: none !important;
+  -moz-user-select: none !important;
+  -ms-user-select: none !important;
+  user-select: none !important;
+  border: 1px solid transparent !important;
+  padding: .375rem .75rem !important;
+  font-size: .9rem !important;
+  line-height: 50px !important;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out !important;
+  background-image: none !important;
+  background: white !important;
+  font-weight: normal !important;
+  text-shadow: none !important;
+  font-size: 1.0em !important;
+  background-image: -webkit-linear-gradient(#ff5757) !important;
+  background-image: linear-gradient(#ff5757) !important;
+  z-index: 1 !important;
+  position:relative !important;
+  left: 50% !important;
+  transform: translate(-50%) !important;
+  margin-top: 50px !important;
+}
+
+.stripe-button-el > span {
+  display: inline-block !important;
+  cursor: pointer !important;
+  width: 90% !important;
+  height: 50px !important;
+  background-color: white !important;
+  color: #ff5757 !important;
+  border-radius: 0px !important;
+  font-weight: 400 !important;
+  text-align: center !important;
+  white-space: nowrap !important;
+  vertical-align: middle !important;
+  -webkit-user-select: none !important;
+  -moz-user-select: none !important;
+  -ms-user-select: none !important;
+  user-select: none !important;
+  border: 1px solid transparent !important;
+  padding: .375rem .75rem !important;
+  font-size: .9rem !important;
+  line-height: 50px !important;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out !important;
+  background-image: none !important;
+  background: white !important;
+  font-weight: normal !important;
+  text-shadow: none !important;
+  font-size: 1.0em !important;
+  background-image: -webkit-linear-gradient(#ff5757) !important;
+  background-image: linear-gradient(#ff5757) !important;
+  z-index: 1 !important;
+  position:relative !important;
+  box-shadow: none !important;
+  left: 50% !important;
+  transform: translate(-50%) !important;
+  margin-top: 50px !important;
+}
 </style>
