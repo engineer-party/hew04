277b8799f6b27f29134cd9317d8fc3f678c57895
せいやーさん <51153166+seiya12@users.noreply.github.com>
2020-07-13 17:41:42 +0900

    ローカル環境ように

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,20 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // UsersTableSeeder::class,
-            // AdminsTableSeeder::class,
+            UsersTableSeeder::class,
+            AdminsTableSeeder::class,
             GenresTableSeeder::class,
-            // ArtistsTableSeeder::class,
-            // MusicsTableSeeder::class,
-            // BuyMusicsTableSeeder::class,
-            // BuyPointsTableSeeder::class,
-            // InformationsTableSeeder::class,
-            // UserInformationsTableSeeder::class,
-            // PlaylistsTableSeeder::class,
-            // PlaylistMusicsTableSeeder::class,
+            ArtistsTableSeeder::class,
+            MusicsTableSeeder::class,
+            BuyMusicsTableSeeder::class,
+            BuyPointsTableSeeder::class,
+            InformationsTableSeeder::class,
+            UserInformationsTableSeeder::class,
+            PlaylistsTableSeeder::class,
+            PlaylistMusicsTableSeeder::class,
             ReportCategoriesTableSeeder::class,
-            // ReportsTableSeeder::class,
-            // GenreMusicTableSeeder::class,
+            ReportsTableSeeder::class,
+            GenreMusicTableSeeder::class,
         ]);
     }
 }
